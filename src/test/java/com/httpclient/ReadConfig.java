879454efdc4fe7b501faa2f444fb7a11f6918a9d
wuchengxu123 <422609624@qq.com>
2020-05-01 10:29:06 +0800

    第二次版本

--- src/test/java/com/httpclient/ReadConfig.java
+++ src/test/java/com/httpclient/ReadConfig.java
@@ -9,7 +9,7 @@ import java.util.Properties;
 import org.testng.annotations.Test;
 //该类从config.properties中读取url
 public class ReadConfig {
-	@Test
+	
 	public  static String getHostOrUrl(String key) throws IOException {
 		InputStream in=new FileInputStream("E:\\eclipse\\testdome2\\src\\test\\java\\config.properties");
 		Properties pr=new Properties();
